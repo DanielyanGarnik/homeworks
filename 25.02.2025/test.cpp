1673f198fa29fecb1be2dac8e28ef90d21f45d4d
DanielyanGarnik <danielyangarnik4@gmail.com>
2025-02-27 12:02:01 +0400

    Update test.cpp

--- 25.02.2025/test.cpp
+++ 25.02.2025/test.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
 
+void sum(){
+	int arajin = 0;
+	int erkrord = 0;
+	std::cout<<"arajin";'\n';
+	std::cin>>arajin;
+	std::cout<<"erkrord";'\n';
+	std::cin>>erkrord;
+	std::cout<<"patasxan"<<arajin + erkrord;'\n';
+}
+int main(){
+	sum();
+	return(0);
+}
