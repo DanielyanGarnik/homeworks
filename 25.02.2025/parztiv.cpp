44bac5f900de66130d0d48ad1dda9162d4977acb
DanielyanGarnik <danielyangarnik4@gmail.com>
2025-02-27 12:09:41 +0400

    Update parztiv.cpp

--- 25.02.2025/parztiv.cpp
+++ 25.02.2025/parztiv.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
 
+void parz(){
+	int a = 0;
+	std::cout<<"nermuteq tiv";'\n';
+	std::cin>>a;
+	a = a%2;
+
+	if(a == 1){
+	std::cout<<"tiv@ parz e";'\n';	
+	}else{
+		std::cout<<"tiv@ hasarak e";'\n';
+	}
+}
+int main(){
+
+	parz();
+}
