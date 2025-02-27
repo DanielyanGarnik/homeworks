64661b71080cbe6b825ce96075aff3b1e4ef2e48
DanielyanGarnik <danielyangarnik4@gmail.com>
2025-02-27 09:44:42 +0400

    Update 2akan.cpp

--- 09.02.2025/2akan.cpp
+++ 09.02.2025/2akan.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+
+int main(){
+  int number = 0;
+  int sum = 0;
+  int b = 0;
+  std::cout<<"nermuteq tiv tasakan hamakargic"<<std::endl;
+  std::cin>>number;
+
+
+  do{
+  b = number / 2;
+  sum = number % 2;
+  std::cout<<sum<<std::endl;
+  number = b;
+  }while(b != 1);
+  std::cout<<"1"<<std::endl;
+}
 
