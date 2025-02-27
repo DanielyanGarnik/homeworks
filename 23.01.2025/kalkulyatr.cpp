cedd6d7e15d4d6662a4a295722bdcb53d5d615de
DanielyanGarnik <danielyangarnik4@gmail.com>
2025-02-27 09:46:17 +0400

    Update kalkulyatr.cpp

--- 23.01.2025/kalkulyatr.cpp
+++ 23.01.2025/kalkulyatr.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 0, b = 0;
+    char c;
+
+    cout << "select number one: ";
+    cin >> a;
+    cout << "select number two: ";
+    cin >> b;
+    cout << "select action (+, -, *, /): ";
+    cin >> c;
+
+    if (c == '+') {
+        cout << "Result: " << a + b << endl;
+    }
+    else if (c == '-') {
+        cout << "Result: " << a - b << endl;
+    }
+    else if (c == '*') {
+        cout << "Result: " << a * b << endl;
+    }
+    else if (c == '/') {
+        if (b != 0) {
+            cout << "Result: " << a / b << endl;
+        } else {
+            cout << "Error! Division by zero is not allowed." << endl;
+        }
+    }
+    else {
+        cout << "Invalid operation!" << endl;
+    }
+
+    return 0;
+}
 
