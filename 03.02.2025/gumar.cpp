4a06ae45c2c23eb35568a1ec335f61829a6b4337
DanielyanGarnik <danielyangarnik4@gmail.com>
2025-03-31 09:07:58 +0400

    Update gumar.cpp

--- 03.02.2025/gumar.cpp
+++ 03.02.2025/gumar.cpp
@@ -5,7 +5,7 @@ int main() {
     std::cout << "mutqagreq tvanshany ";
     std::cin >> number;
     sum = (number / 1000) + ((number / 100) % 10) + ((number / 10) % 10) + (number % 10);
-    std::cout << "ariunq" << sum <<std::endl;
+    std::cout << "ardiunq" << sum <<std::endl;
     return 0;
 }
 
